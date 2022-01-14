f0fec255117285b451d51be4e99ef47124945df2
Arkell Kenney <kell@Arkells-MacBook-Air.local>
2022-01-14 13:35:58 -0500

    Refactor GenInfo.js

--- cv-project/src/components/GenInfo.js
+++ cv-project/src/components/GenInfo.js
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import EditableLabel from "./EditableLabel";
 
-class GenInfo extends Component {
-  render() {
-    return (
+const GenInfo = () => {
+  return (
+    <div>
       <div>
-        <div>
-          <EditableLabel value="Full Name" tag="h1" />
-          <EditableLabel value="Phone Number" tag="h3" />
-          <EditableLabel value="E-mail" tag="h4" />
-        </div>
+        <EditableLabel value="Full Name" tag="h1" />
+        <EditableLabel value="Phone Number" tag="h3" />
+        <EditableLabel value="E-mail" tag="h4" />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default GenInfo;
